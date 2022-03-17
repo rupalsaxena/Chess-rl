9373befb623e8c4f66ca5acb206e3f005ecce0e8
Euxane Vaz Pinto <62841919+Eu-x@users.noreply.github.com>
2022-03-17 13:05:40 +0100

    RELU instead of sigmoid

--- src/main/utils/NN.py
+++ src/main/utils/NN.py
@@ -6,10 +6,11 @@ class NN:
         self.h = helpers()
 
     def Forwardprop(self, x, W1, b1, W2, b2):
+
         hid_layer = np.dot(x, W1)+b1
-        hid_layer_act = self.h.sig(hid_layer)
+        hid_layer_act = np.maximum(hid_layer)
         out_layer=np.dot(hid_layer_act, W2)+b2
-        out_layer_act =self.h.sig(out_layer)
+        out_layer_act =np.maximum(out_layer)
         
         return out_layer_act, hid_layer_act
     
@@ -18,13 +19,14 @@ class NN:
     #W1 : as the features are represented with all the 58 entries (unlike the action taken), all weights are updated (?)
     def Backpropagation(self, eta, a_agent, delta, out_layer_act, hid_layer_act, x,  W1, W2, b1, b2):
         
-        delta_W2=eta*delta*(hid_layer_act * (1-hid_layer_act))
-        delta_W1=eta*np.outer(x, (delta*W2[:, a_agent]*(hid_layer_act *(1-hid_layer_act))).T)
+        delta_W2=eta*delta*out_layer_act[a_agent]
+        delta_W1=eta*np.outer(x, (delta*W2[:, a_agent]*(self.h.reluder(hid_layer_act))))
 
         W2[:,a_agent]=W2[:,a_agent]+eta*delta_W2
         b2[a_agent]=b2[a_agent]+eta*delta
         
         W1=W1+eta*delta_W1
-        b1=b1+eta*delta*(W2[:,a_agent]).dot((hid_layer_act *(1-hid_layer_act)))                  
+        b1=b1+eta*delta*(W2[:,a_agent])*self.h.reluder(hid_layer_act)
+        
 
         return  W1,  W2[:, a_agent], b1, b2[a_agent]
